24b8c931a1b6ba7046bf3c320b4f311365e717c2
Sam Windham <sam.windham95@gmail.com>
2019-07-17 16:34:17 +0200

    Fix directory path and add SPY to script

--- download_historical.py
+++ download_historical.py
@@ -6,7 +6,7 @@ from api import yahoo
 
 def save_ticker_historical(ticker):
     historical = yahoo.get_daily(ticker)
-    historical.to_csv(os.path.join(os.path.dirname(__file__), 'data/spy/historical/{ticker}.csv'.format(ticker=ticker)))
+    historical.to_csv(os.path.join(os.path.dirname(__file__), 'data/price/{ticker}.csv'.format(ticker=ticker)))
 
 
 def save_all_historical(tickers):
@@ -19,3 +19,4 @@ ticker_csv_path = os.path.join(os.path.dirname(__file__), 'data/spy/tickers.csv'
 tickers = pd.read_csv(ticker_csv_path, header=None)[1]
 
 save_all_historical(tickers)
+save_ticker_historical('SPY')
