f57c532f412d764979b4f831e79875d8fb5c0d3d
Sam Windham <sam.windham95@gmail.com>
2019-07-17 16:28:51 +0200

    Implemnt and download data from Yahoo Finance

--- api/yahoo.py
+++ api/yahoo.py
@@ -1,39 +1,6 @@
-import requests
-import re
-import io
-from datetime import datetime, timedelta
-
 import pandas as pd
+import yfinance as yf
 
 
-class YahooFinanceHistory:
-    timeout = 2
-    crumb_link = 'https://finance.yahoo.com/quote/{0}/history?p={0}'
-    crumble_regex = r'CrumbStore":{"crumb":"(.*?)"}'
-    quote_link = 'https://query1.finance.yahoo.com/v7/finance/download/{quote}?period1={dfrom}&period2={dto}&interval=1d&events=history&crumb={crumb}'
-
-    def __init__(self, symbol, days_back=7):
-        self.symbol = symbol
-        self.session = requests.Session()
-        self.dt = timedelta(days=days_back)
-
-    def get_crumb(self):
-        response = self.session.get(self.crumb_link.format(self.symbol), timeout=self.timeout)
-        response.raise_for_status()
-        match = re.search(self.crumble_regex, response.text)
-
-        if not match:
-            raise ValueError('Could not get crumb from Yahoo Finance')
-        else:
-            self.crumb = match.group(1)
-
-    def get_quote(self):
-        if not hasattr(self, 'crumb') or len(self.session.cookies) == 0:
-            self.get_crumb()
-        now = datetime.utcnow()
-        dateto = int(now.timestamp())
-        datefrom = int((now - self.dt).timestamp())
-        url = self.quote_link.format(quote=self.symbol, dfrom=datefrom, dto=dateto, crumb=self.crumb)
-        response = self.session.get(url)
-        response.raise_for_status()
-        return pd.read_csv(io.StringIO(response.text), parse_dates=['Date'])
+def get_daily(ticker):
+    return yf.download(ticker, period="max", interval="1d")
