1bdd521f2d379012fb667852a572784de3be3ea6
Sam Windham <sam.windham95@gmail.com>
2019-08-27 14:05:40 +0200

    refactor download_info

--- tools/download_info.py
+++ tools/download_info.py
@@ -8,20 +8,30 @@ from api import yahoo
 from data.info import info
 
 
-def save_info(tickers):
+def download_info(ticker):
+    return yahoo.get_info(ticker)
+
+
+def download_all_info(tickers):
     info_dict = {}
     for i, ticker in enumerate(tickers):
         print(i, '-', ticker)
-        info_t = yahoo.get_info(ticker)
+        info_t = download_info(ticker)
         info_dict[ticker] = info_t
+    return info_dict
 
-    print('---- SAVING ----')
+
+def save_info(info_dict):
     info.save(info_dict)
-    print('Done.')
 
 
 if __name__ == '__main__':
     ticker_csv_path = os.path.join(os.path.dirname(__file__), '../data/spy/tickers.csv')
     tickers = pd.read_csv(ticker_csv_path, header=None)[1]
 
-    save_info(tickers)
+    print('---- DOWNLOADING ----')
+    info_dict = download_info(tickers)
+    print('---- SAVING ----')
+    save_info(info_dict)
+
+    print('Done.')
