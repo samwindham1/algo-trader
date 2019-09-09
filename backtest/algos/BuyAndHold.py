78562f74a359a76d7bbff01bae4e470300bcc615
Sam Windham <sam.windham95@gmail.com>
2019-09-09 09:59:57 +0200

    Handle equal hodlings of mutlple datafeeds

--- backtest/algos/BuyAndHold.py
+++ backtest/algos/BuyAndHold.py
@@ -2,6 +2,22 @@ from . import BaseStrategy as base
 
 
 class BuyAndHold(base.Strategy):
+    params = (
+        ('target_percent', 0.99),
+    )
+
+    def __init__(self):
+        base.Strategy.__init__(self)
+
+    def buy_and_hold(self):
+        for d in self.datas:
+            split_target = self.params.target_percent / len(self.datas)
+            self.order_target_percent(d, target=split_target)
+
     def next(self):
-        if not self.getposition(self.data).size:
-            self.order_target_percent(self.data, target=0.99)
+        if not self.position:
+            self.buy_and_hold()
+
+        elif self.order_rejected:
+            self.buy_and_hold()
+            self.order_rejected = False
