9ed4aec482977ecc41c31811ccaa7212cbc54873
Sam Windham <sam.windham95@gmail.com>
2019-10-18 14:58:24 +0200

    async support for multiple tickers

--- api/yahoo.py
+++ api/yahoo.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 from datetime import date
+from tools.log import log
 
 
 def get_daily(ticker, start=None):
@@ -8,6 +9,13 @@ def get_daily(ticker, start=None):
         return yf.download(ticker, period='max', interval='1d')
     else:
         return yf.download(ticker, start=start, end=date.today(), interval='1d')
+
+
+def get_daily_async(tickers, start=None):
+    if start == None:
+        return yf.download(tickers, period='max', interval='1d', group_by="ticker")
+    else:
+        return yf.download(tickers, start=start, end=date.today(), interval='1d', group_by="ticker")
 
 
 def get_info(ticker):
@@ -22,11 +30,18 @@ def get_info(ticker):
     except IndexError as e:
         print('ERROR:', ticker)
         print(e)
+        log.log(type(e).__name__, e)
         return info
 
-    balance_sheet = balance_sheet.iloc[:, 0]
-    financials = financials.iloc[:, 0]
-    cashflow = cashflow.iloc[:, 0]
+    try:
+        balance_sheet = balance_sheet.iloc[:, 0]
+        financials = financials.iloc[:, 0]
+        cashflow = cashflow.iloc[:, 0]
+    except AttributeError as e:
+        print('ERROR:', ticker)
+        print(e)
+        log.log(type(e).__name__, e)
+        return info
 
     return_info = pd.concat([info, balance_sheet, financials, cashflow], axis=0)
     print(ticker)
